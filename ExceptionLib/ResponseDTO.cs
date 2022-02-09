cc9ecdade7225c8cd2191074e8c04ddfbe2b5d22
DESKTOP-QSOECB8\oscar <oscarlozanopa@gmail.com>
2022-02-08 20:50:58 -0500

    Clase obtencion respuesta como objeto

---
+++ ExceptionLib/ResponseDTO.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ExceptionLib
+{
+    public class ResultDto
+    {
+        public ResultDto() { }
+        public ResultDto(IActionResult result)
+        {
+            var jsonResult = result as JsonResult;
+            var origin = JsonConvert.DeserializeObject<ResultDto>(JsonConvert.SerializeObject(jsonResult.Value));
+            this.response = origin.response;
+            this.successful = origin.successful;
+            this.message = origin.message;
+            this.data = origin.data;
+            this.error = origin.data;
+        }
+
+        public bool response { get; set; }
+
+        public bool successful { get; set; }
+
+        public dynamic data { get; set; }
+
+        public string message { get; set; }
+
+        public string error { get; set; }
+    }
+}
